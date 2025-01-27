827d03aa9126df60c27fac74f4647e09630f14b4
graziana-ciccarelli <grazianaciccarelli99@gmail.com>
2025-01-27 15:01:47 +0100

    snack 1-2 completati

---
+++ ex-js-array-methods-main/snack-1/script.js
@@ -0,0 +1,5 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+console.log(names.map(name => `'${name}'`).join(', '));
