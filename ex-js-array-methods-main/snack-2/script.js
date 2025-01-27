f88f892e897d0d8da55278606bf9a7ece24f0a1e
Graziana Ciccarelli <grazianaciccarelli99@gmail.com>
2025-01-27 15:45:03 +0100

    Update script.js

--- ex-js-array-methods-main/snack-2/script.js
+++ ex-js-array-methods-main/snack-2/script.js
@@ -6,4 +6,6 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
-console.log(people.map(person => `'${person.name}'`).join(', '));
+people.forEach(person => {
+  console.log(person.name);
+});
